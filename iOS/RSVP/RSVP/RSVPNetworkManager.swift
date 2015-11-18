b9701c6a6a0718a28ba33ef844833e5272b6bcfc
Oliver Dormody <odormody@pandora.com>
2015-11-18 15:53:47 -0800

    iOS: added login method

--- iOS/RSVP/RSVP/RSVPNetworkManager.swift
+++ iOS/RSVP/RSVP/RSVPNetworkManager.swift
@@ -9,11 +9,23 @@
 import Foundation
 import AFNetworking
 
+typealias NetworkCompletionBlock = (Any?, NSError?)->Void
+
 class RSVPNetworkManager {
     static let instance = RSVPNetworkManager()
-    private let manager = AFHTTPRequestOperationManager(baseURL: NSURL(string: "asdada.com"))
+    private let manager = AFHTTPRequestOperationManager(baseURL: NSURL(string: "http://aai.savagebeast.com:9000/api/"))
 
-    func loginUser(🎅🏽: String, 🔑: String) -> Bool {
-
+    func loginUser(🎅🏽: String, 🔑: String, 😊: NetworkCompletionBlock, 😞: NetworkCompletionBlock) -> Bool {
+        let 🌐 = "\(manager.baseURL?.absoluteString)/login"
+        var 🎉 = false
+        manager.POST(🌐, parameters: ["uid":🎅🏽, "password":🔑], success: {(📡, 🍕) -> Void in
+            🎉 = true
+            😊(🍕, nil)
+        }){(📡, 💣) -> Void in
+            🎉 = false
+            😞(nil, 💣)
+        }
+        
+        return 🎉
     }
 }
