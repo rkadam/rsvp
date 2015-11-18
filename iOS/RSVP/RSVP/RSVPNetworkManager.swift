ef2c4656de926b78362d5713844be42c91a520dd
jkuai <jkuai@pandora.com>
2015-11-18 10:58:13 -0800

    set up the network manager

---
+++ iOS/RSVP/RSVP/RSVPNetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  RSVPNetworkManager.swift
+//  RSVP
+//
+//  Created by Jianghua Kuai on 11/18/15.
+//  Copyright © 2015 Pandora. All rights reserved.
+//
+
+import Foundation
+import AFNetworking
+
+class RSVPNetworkManager {
+    
+}
