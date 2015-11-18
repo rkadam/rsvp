034995683bbe1c59f97d3e73b8352382c4310ae5
Oliver Dormody <odormody@pandora.com>
2015-11-18 13:25:09 -0800

    Updated iOS login UI

---
+++ iOS/RSVP/RSVP/RSVPLoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RSVPLoginViewController.swift
+//  RSVP
+//
+//  Created by Oliver Dormody on 11/18/15.
+//  Copyright © 2015 Pandora. All rights reserved.
+//
+
+import UIKit
+
+class RSVPLoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func loginButtonTapped(sender: UIButton) {
+        
+    }
+    
+    
+}
+
