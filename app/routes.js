6330a3369c7923d98938ffe673faff5bc4ce78ee
Mike Petrovich <michael.c.petrovich@gmail.com>
2015-11-19 00:48:25 -0500

    App: Fix default route to support Angular UI router

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,7 @@
 
 'use strict';
 
+var path = require('path');
 // var errors = require('./components/errors');
 
 module.exports = function(app) {
@@ -42,13 +43,9 @@ module.exports = function(app) {
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(send404);
   // All other routes should redirect to the index.html
-  app.route('/')
-    .get(function(req, res) {
-      res.sendfile(app.get('appPath') + '/index.html');
-    });
-
   app.route('/*')
     .get(function(req, res) {
-      res.redirect('/');
+      var indexFilepath = path.resolve(path.join(app.get('appPath'), 'index.html'));
+      res.sendfile(indexFilepath);
     });
 };
