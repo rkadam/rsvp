93ddd4fd7c9c9d5d89e9b4268805882ccd02b4f3
Mark Wu <Mark.Wu@pandora.com>
2015-11-19 09:25:26 -0800

    more stuff

--- Android/RSVP/app/src/main/java/com/pandora/rsvp/ui/CreateInvitationActivity.java
+++ Android/RSVP/app/src/main/java/com/pandora/rsvp/ui/CreateInvitationActivity.java
@@ -8,7 +8,21 @@ import android.view.MenuItem;
 import com.pandora.rsvp.R;
 import com.pandora.rsvp.ui.base.BaseActivity;
 
+import butterknife.BindString;
+
 public class CreateInvitationActivity extends BaseActivity {
+    @BindString(R.string.create_invitation_activity_title) String title;
+    
+    @Override
+    protected int getActivityLayoutRes() {
+        return R.layout.activity_create_invitation;
+    }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(title);
+    }
 
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
@@ -16,12 +30,7 @@ public class CreateInvitationActivity extends BaseActivity {
 //        getMenuInflater().inflate(R.menu.menu_create_invitation, menu);
 //        return true;
 //    }
-
-    @Override
-    protected int getActivityLayoutRes() {
-        return R.layout.activity_create_invitation;
-    }
-
+//    
 //    @Override
 //    public boolean onOptionsItemSelected(MenuItem item) {
 //        // Handle action bar item clicks here. The action bar will
