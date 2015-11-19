16b1fc37542ef80097cd23b0301378473ffa2a90
Oliver Dormody <odormody@pandora.com>
2015-11-19 12:33:10 -0800

    iOS responder cell

--- iOS/RSVP/RSVP/RSVPResponderCollectionViewCell.swift
+++ iOS/RSVP/RSVP/RSVPResponderCollectionViewCell.swift
@@ -11,22 +11,25 @@ import UIKit
 class RSVPResponderCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView! {
         didSet {
-            self.imageView.layer.masksToBounds = true
-            self.imageView.layer.cornerRadius = 25
+            imageView.layer.masksToBounds = true
+            imageView.layer.cornerRadius = 25
         }
     }
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var userDepartmentLabel: UILabel!
     
-    var responder: RSVPResponder {
+    var responder: RSVPResponder! {
         didSet {
+            let data = NSData(contentsOfURL : NSURL(string: responder.imageURL)!)
+            imageView.image = UIImage(data : data!)
             
+            userNameLabel.text = responder.name
+            userDepartmentLabel.text = responder.department
         }
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         self.backgroundColor = UIColor.whiteColor()
     }
 }
