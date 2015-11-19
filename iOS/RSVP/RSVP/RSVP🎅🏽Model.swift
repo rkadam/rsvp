63ae4ee5f242121e3e4ff73f35aaaaa879753672
Oliver Dormody <odormody@pandora.com>
2015-11-19 10:14:04 -0800

    iOS user model work

---
+++ iOS/RSVP/RSVP/RSVP🎅🏽Model.swift
@@ -0,0 +1,13 @@
+//
+//  RSVP🎅🏽Model.swift
+//  RSVP
+//
+//  Created by Oliver Dormody on 11/19/15.
+//  Copyright © 2015 Pandora. All rights reserved.
+//
+
+import Foundation
+
+class RSVP🎅🏽Model {
+    
+}
