8c7276c0d108ac050316741ff74b30326e8a1005
Mike Petrovich <michael.c.petrovich@gmail.com>
2015-11-20 10:52:43 -0500

    Web: Cleanup

--- web/src/components/rsvpDeptChart/rsvpDeptChart.js
+++ web/src/components/rsvpDeptChart/rsvpDeptChart.js
@@ -1,69 +1,69 @@
 angular.module('rsvp').directive('rsvpDeptChart', function() {
-  'use strict';
-  return {
-    restrict: 'E',
-    scope: {
-      invite:'=',
-      selected: '=',
-    },
-    templateUrl: 'components/rsvpDeptChart/rsvpDeptChart.html',
-    link: function(scope, iElement, attrs) {
-      scope.options = {
-        chart: {
-          type: 'pieChart',
-          donut: true,
-          donutRatio: 0.4,
-          // height: 300,
-          margin : {
-            top: 20,
-            right: 20,
-            bottom: 50,
-            left: 55
-          },
-          x: function(d){return d.label;},
-          y: function(d){return d.value;},
-          showValues: true,
-          valueFormat: function(d){
-              return parseInt(d);
-          },
-          color: function(d, i) {
-            var color = d3.rgb('#224099').brighter(i*0.7);
-            return color;
-          },
-          legendPosition: 'right',
-          duration: 500,
-          showLabels: false,
-          xAxis: {
-            tickFormat : function(d) {
-              return d !== '0' ? d : '< 1';
-            }
-          },
-        }
-      };
+	'use strict';
+	return {
+		restrict: 'E',
+		scope: {
+			invite:'=',
+			selected: '=',
+		},
+		templateUrl: 'components/rsvpDeptChart/rsvpDeptChart.html',
+		link: function(scope, iElement, attrs) {
+			scope.options = {
+				chart: {
+					type: 'pieChart',
+					donut: true,
+					donutRatio: 0.4,
+					// height: 300,
+					margin : {
+						top: 20,
+						right: 20,
+						bottom: 50,
+						left: 55
+					},
+					x: function(d){return d.label;},
+					y: function(d){return d.value;},
+					showValues: true,
+					valueFormat: function(d){
+							return parseInt(d);
+					},
+					color: function(d, i) {
+						var color = d3.rgb('#224099').brighter(i*0.7);
+						return color;
+					},
+					legendPosition: 'right',
+					duration: 500,
+					showLabels: false,
+					xAxis: {
+						tickFormat : function(d) {
+							return d !== '0' ? d : '< 1';
+						}
+					},
+				}
+			};
 
-      scope.data = [];
-      var updateData = function() {
-        if (! scope.invite) { return; }
-        scope.data.length = 0;
-        var departments = {};
-        scope.invite.responses.forEach(function(response) {
-          if( scope.selected && ! response.selected) {
-            return;
-          }
-          var dept = response.department;
-          if (! departments[dept]) {
-            departments[dept] = 0;
-          }
-          departments[dept]++;
-        });
-        var values = Object.keys(departments).map(function(dept) {
-          return {label: dept, value: departments[dept]};
-        });
-        scope.data = values;
-      };
+			scope.data = [];
+			var updateData = function() {
+				if (! scope.invite) { return; }
+				scope.data.length = 0;
+				var departments = {};
+				scope.invite.responses.forEach(function(response) {
+					if( scope.selected && ! response.selected) {
+						return;
+					}
+					var dept = response.department;
+					if (! departments[dept]) {
+						departments[dept] = 0;
+					}
+					departments[dept]++;
+				});
+				var values = Object.keys(departments).map(function(dept) {
+					return {label: dept, value: departments[dept]};
+				});
+				scope.data = values;
+			};
 
-      scope.$watch('invite.responses', updateData);
-      scope.$watch('selected', updateData);
-    }
-  };
+			scope.$watch('invite.responses', updateData);
+			scope.$watch('selected', updateData);
+		}
+	};
 });
--- web/src/components/rsvpTenureChart/rsvpTenureChart.js
+++ web/src/components/rsvpTenureChart/rsvpTenureChart.js
@@ -1,70 +1,70 @@
 angular.module('rsvp').directive('rsvpTenureChart', function() {
-  'use strict';
-  return {
-    restrict: 'E',
-    scope: {
-      invite:'=',
-      selected: '=',
-    },
-    templateUrl: 'components/rsvpTenureChart/rsvpTenureChart.html',
-    link: function(scope, iElement, attrs) {
-      scope.options = {
-        chart: {
-          type: 'discreteBarChart',
-          // height: 450,
-          margin : {
-            top: 20,
-            right: 20,
-            bottom: 50,
-            left: 55
-          },
-          x: function(d){return d.label;},
-          y: function(d){return d.value;},
-          showValues: true,
-          valueFormat: function(d){
-              return parseInt(d);
-          },
-          color: function(d, i) {
-            var color = d3.rgb('#224099').brighter(i*0.7);
-            return color;
-          },
-          duration: 500,
-          showYAxis: false,
-          xAxis: {
-            tickFormat : function(d) {
-              return d !== '0' ? d : '< 1';
-            }
-          },
-        }
-      };
+	'use strict';
+	return {
+		restrict: 'E',
+		scope: {
+			invite:'=',
+			selected: '=',
+		},
+		templateUrl: 'components/rsvpTenureChart/rsvpTenureChart.html',
+		link: function(scope, iElement, attrs) {
+			scope.options = {
+				chart: {
+					type: 'discreteBarChart',
+					// height: 450,
+					margin : {
+						top: 20,
+						right: 20,
+						bottom: 50,
+						left: 55
+					},
+					x: function(d){return d.label;},
+					y: function(d){return d.value;},
+					showValues: true,
+					valueFormat: function(d){
+							return parseInt(d);
+					},
+					color: function(d, i) {
+						var color = d3.rgb('#224099').brighter(i*0.7);
+						return color;
+					},
+					duration: 500,
+					showYAxis: false,
+					xAxis: {
+						tickFormat : function(d) {
+							return d !== '0' ? d : '< 1';
+						}
+					},
+				}
+			};
 
-      scope.data = [{
-         key: "Years at Pandora",
-         values: []
-      }];
-      var updateData = function() {
-        if (! scope.invite) { return; }
-        var years = {};
-        scope.data[0].values.length = 0;
-        scope.invite.responses.forEach(function(response) {
-          var year = Math.floor(response.years);
-          if( scope.selected && ! response.selected) {
-            return;
-          }
-          if (! years[year]) {
-            years[year] = 0;
-          }
-          years[year]++;
-        });
-        var values = Object.keys(years).map(function(year) {
-          return {label: year, value: years[year]};
-        });
-        scope.data[0].values = values;
-      };
+			scope.data = [{
+				 key: "Years at Pandora",
+				 values: []
+			}];
+			var updateData = function() {
+				if (! scope.invite) { return; }
+				var years = {};
+				scope.data[0].values.length = 0;
+				scope.invite.responses.forEach(function(response) {
+					var year = Math.floor(response.years);
+					if( scope.selected && ! response.selected) {
+						return;
+					}
+					if (! years[year]) {
+						years[year] = 0;
+					}
+					years[year]++;
+				});
+				var values = Object.keys(years).map(function(year) {
+					return {label: year, value: years[year]};
+				});
+				scope.data[0].values = values;
+			};
 
-      scope.$watch('invite.responses', updateData);
-      scope.$watch('selected', updateData);
+			scope.$watch('invite.responses', updateData);
+			scope.$watch('selected', updateData);
 
-    }
-  };
+		}
+	};
 });
