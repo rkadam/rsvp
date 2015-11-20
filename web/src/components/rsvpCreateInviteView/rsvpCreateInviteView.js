152132499d1dc2cddd80cdd10bba1112525e3f6e
Mike Petrovich <michael.c.petrovich@gmail.com>
2015-11-20 15:10:09 -0500

    Web: Remove default email list

--- web/src/components/rsvpCreateInviteView/rsvpCreateInviteView.js
+++ web/src/components/rsvpCreateInviteView/rsvpCreateInviteView.js
@@ -28,7 +28,6 @@ angular.module('rsvp').directive('rsvpCreateInviteView', function(
 			ctrl.invite.title = $stateParams.title;
 			ctrl.invite.method = 'random';
 			ctrl.invite.response_accept_limit = 1;
-			ctrl.invite.email_to = 'rsvp@pandora.com';
 			ctrl.invite.rsvp_by_date = rsvpDatetime;
 			ctrl.invite.rsvp_by_time = rsvpDatetime;
 
